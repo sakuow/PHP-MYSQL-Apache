db851cd3c6f75b06790eee9fad2c0c72271fab66
sakuow <yusaku1335@gmail.com>
2021-12-21 17:31:00 +0900

    変数定義について

--- 030_PHP基礎Part1/0200_コメントの記法/start/index.php
+++ 030_PHP基礎Part1/0200_コメントの記法/start/index.php
@@ -1,6 +1,6 @@
 <?php
-echo 'hello'
-?>
+echo 'hello';
+
 
 /*
 echo 'hello'
--- 030_PHP基礎Part1/0300_変数定義/start/index.php
+++ 030_PHP基礎Part1/0300_変数定義/start/index.php
@@ -1,1 +1,4 @@
-<?php 
+<?php
+$person_name = 'Tim';
+echo 'hello, ' . $person_name . '<br />';
+echo 'bye, ' . $person_name;
