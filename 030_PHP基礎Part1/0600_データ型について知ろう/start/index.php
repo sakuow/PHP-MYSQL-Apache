3cc3eb6b3dbb7ad7cde509b6ea0b1ec712c05055
sakuow <yusaku1335@gmail.com>
2021-12-23 11:58:27 +0900

    データ型について

--- 030_PHP基礎Part1/0600_データ型について知ろう/start/index.php
+++ 030_PHP基礎Part1/0600_データ型について知ろう/start/index.php
@@ -1,1 +1,20 @@
-<?php 
+<?php
+// データ型とは
+$i = 1;
+// PHPはbool方は大文字小文字どちらも可
+$b = true;
+$str = 'hello';
+// データの確認方法 var_dump
+var_dump($i);
+var_dump($str);
+var_dump($b);
+
+// 異なる方は自動的に変換される。
+echo $i + $b;
+
+
+// (データ型)を記述することで手動で変換することもできる。
+echo $i + (int) $b;
+
+// 型の取り扱いの注意点
+var_dump($i === '1');
