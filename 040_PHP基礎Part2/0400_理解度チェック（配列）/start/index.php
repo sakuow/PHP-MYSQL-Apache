c8ff2f947a650019441f8cd9c01ed71e6a518e25
sakuow <yusaku1335@gmail.com>
2021-12-24 16:05:58 +0900

    配列確認問題

--- 040_PHP基礎Part2/0400_理解度チェック（配列）/start/index.php
+++ 040_PHP基礎Part2/0400_理解度チェック（配列）/start/index.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
  * 商品名 => [価格, 在庫数]
  */
 $products = [
-    'table' => [1000, 2], 
+    'table' => [1000, 2],
     'chair' => [500, 4],
     'bed' => [10000, 1],
     'light' => [5000, 1]
@@ -11,7 +12,7 @@ $products = [
 
 /**
  * 問１：商品一覧
- * 
+ *
  * 商品一覧
  * tableは1000円で2個存在します。
  * chairは500円で4個存在します。
@@ -19,16 +20,18 @@ $products = [
  * lightは5000円で1個存在します。
  */
 echo '<div>商品一覧</div>';
+foreach ($products as $key => $val) {
+    echo "{$key}は{$val[0]}円で{$val[1]}個存在します。<br />";
+}
 
 
- 
 /**
  * 問２：商品購入
- * 
+ *
  * $cartの品物を買いたいと想定して、
  * $productsの在庫数が足りている場合、足りていない場合で
  * 以下のように画面に表示してください。
- * 
+ *
  * 商品購入
  * tableを1個ください。
  * はい。ありがとうございます。 <- 足りている場合
@@ -42,3 +45,21 @@ $cart = [
     'bed' => 2,
 ];
 echo '<div>商品購入</div>';
+// if ($cart['table'] >= $products['table'][1]) {
+//     echo 'はい。ありがとうございます。';
+// }
+// if ($cart['bed'][0] <= $products['bed'][1]) {
+//     echo "すいません。ベッドは{$products['bed'][1]}しかありません。";
+// }
+
+foreach ($cart as $key => $val) {
+
+    echo "{$key}を{$val}個ください。";
+
+    $p_num = $products[$key][1];
+    if ($val <= $p_num) {
+        echo 'はい。ありがとうございます。';
+    } else {
+        echo "すいません。{$key}は{$p_num}しかありません。";
+    }
+}
