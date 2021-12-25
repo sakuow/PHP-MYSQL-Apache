af8654184f965114c17aefa81287d836c279a630
sakuow <yusaku1335@gmail.com>
2021-12-25 17:35:51 +0900

    定数について

--- 050_PHP基礎Part3/0100_プログラムの記述順には注意しよう/start/index.php
+++ 050_PHP基礎Part3/0100_プログラムの記述順には注意しよう/start/index.php
@@ -1,5 +1,15 @@
 <?php
-function counter($step = 1) {
+
+/**
+ * プログラムの注意すべき記述順について
+ * 
+ * 関数ないの処理は関数が実行されて初めて動く
+ * 関数宣言はプログラムの実行よりも前に準備される
+ * それ以外は上から順に実行される
+ */
+
+function counter($step = 1)
+{
     global $num;
     $num += $step;
     echo $num;
--- 050_PHP基礎Part3/0300_定数を使ってみよう/start/index.php
+++ 050_PHP基礎Part3/0300_定数を使ってみよう/start/index.php
@@ -1,19 +1,37 @@
 <?php
+
 /**
- * プログラムの注意すべき記述順について
- * 
- * 関数ないの処理は関数が実行されて初めて動く
- * 関数宣言はプログラムの実行よりも前に準備される
- * それ以外は上から順に実行される
+ * 定数の使い方
+ *
+ * - define, constでの定義方法
+ * - constはif文や関数の中では使えない
+ * - defineではグローバルスコープに値が配置される
+ * - constは名前空間内に配置される（名前空間のレクチャーで紹介）
  */
 
- function counter($step = 1) {
-   global $num;
-   $num += $step;
-   echo $num;
-   returen $num;
- }
+//  function counter($step = 1) {
+//    global $num;
+//    $num += $step;
+//    echo $num;
+//    returen $num;
+//  }
 
- $num = 0;
+//  $num = 0;
 // counter関数が実行されているのは $numが定義された後になっている。
- counter(2);
+//  counter(2);
+
+if (!defined('TAX_RATE')) {
+  // const TAX_RATE = 0.1;
+  define('TAX_RATE', 0.1);
+}
+
+
+// const TAX_RATE = 0.1;
+function with_tax($base_price, $tax_rate = TAX_RATE)
+{
+  $sum_price = $base_price + ($base_price * $tax_rate);
+  $sum_price = round($sum_price);
+  return $sum_price;
+}
+$price = with_tax(1000, 0.08);
+echo $price;
