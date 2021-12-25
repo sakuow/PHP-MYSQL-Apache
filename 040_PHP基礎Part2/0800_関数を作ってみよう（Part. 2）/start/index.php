93bdb714229617135fc5169c24f94929cb80a576
sakuow <yusaku1335@gmail.com>
2021-12-25 15:47:34 +0900

    関数を作るpart2

--- 040_PHP基礎Part2/0800_関数を作ってみよう（Part. 2）/start/index.php
+++ 040_PHP基礎Part2/0800_関数を作ってみよう（Part. 2）/start/index.php
@@ -1,1 +1,14 @@
 <?php
+
+$price = 1000;
+
+// $tax_rate = 0.1　はデフォルト引数といいfunctionにより変数を作成する際に値を設定しておくことができる。
+function with_tax($base_price, $tax_rate = 0.1)
+{
+  $sum_price = $base_price + ($base_price * $tax_rate);
+  $sum_price = round($sum_price);
+  return $sum_price;
+}
+
+$price = with_tax($price);
+echo "購入金額は{$price}です。";
