7ef91732f23604f0d9e07e2b39223fd9348fc02a
sakuow <yusaku1335@gmail.com>
2021-12-25 21:23:35 +0900

    名前空間について

---
+++ 050_PHP基礎Part3/0600_名前空間について学ぼう/start/lib.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace lib;
+
+const TAX_RATE = 0.1;
+
+if (!defined('TAX_RATE')) {
+  define('TAX_RATE', 0.1);
+}
+
+function with_tax($base_price, $tax_rate = TAX_RATE)
+{
+  $sum_price = $base_price + ($base_price * $tax_rate);
+  $sum_price = round($sum_price);
+  return $sum_price;
+}
