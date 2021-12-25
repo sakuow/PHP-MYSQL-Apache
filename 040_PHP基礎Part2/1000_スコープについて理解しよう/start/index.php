5dd4e18cc62f18e9269603848cf3bd50b8404b79
sakuow <yusaku1335@gmail.com>
2021-12-25 16:19:50 +0900

    スコープについて

--- 040_PHP基礎Part2/1000_スコープについて理解しよう/start/index.php
+++ 040_PHP基礎Part2/1000_スコープについて理解しよう/start/index.php
@@ -1,1 +1,35 @@
 <?php
+
+/**
+ * スコープについて
+ * 変数が参照可能な範囲
+ */
+// グローバルスコープ
+$a = 0;
+echo $a;
+
+// ローカルスコープ
+function fn1()
+{
+  $b = 1;
+}
+
+function fn2()
+{
+  // global $変数名とすることでfunction内でグローバル変数を再度定義し使用できるようになる。
+  global $a;
+  $b = 2;
+  echo $a;
+}
+
+// スーパーグローバル
+function fn2()
+{
+  global $a;
+  // PHP内で
+  var_dump($_SERVER);
+  // $b = 2;
+  // echo $a;
+}
+
+fn2();
