a1db8b2fa874adcd0af768dbf5bc94336be894e7
sakuow <yusaku1335@gmail.com>
2021-12-27 11:06:17 +0900

    名前空間

--- 050_PHP基礎Part3/0600_名前空間について学ぼう/start/lib.php
+++ 050_PHP基礎Part3/0600_名前空間について学ぼう/start/lib.php
@@ -4,10 +4,8 @@ namespace lib;
 
 const TAX_RATE = 0.1;
 
-if (!defined('TAX_RATE')) {
-  define('TAX_RATE', 0.1);
-}
-
+// $tax_rate = TAX_RATEの部分は同じ名前空間ないにある場合は\libは必要ない。
+// 他の名前空間から参照して使用する場合に＼libが必要となる。
 function with_tax($base_price, $tax_rate = TAX_RATE)
 {
   $sum_price = $base_price + ($base_price * $tax_rate);
