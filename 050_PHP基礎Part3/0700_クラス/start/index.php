26fe1fe3d5b199129661ac227376bab55fbcfeff
sakuow <yusaku1335@gmail.com>
2021-12-31 11:15:18 +0900

    クラスについて

--- 050_PHP基礎Part3/0700_クラス/start/index.php
+++ 050_PHP基礎Part3/0700_クラス/start/index.php
@@ -1,10 +1,24 @@
-<?php 
+<?php
+
+/**
+ * クラスの基礎
+ */
 class Person
 {
     private $name;
     public $age;
 
-    function hello() {
+    function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    function hello()
+    {
         echo 'hello, ' . $this->name;
     }
 }
+
+$bob = new Person('Bob', 18);
+$bob->hello();
