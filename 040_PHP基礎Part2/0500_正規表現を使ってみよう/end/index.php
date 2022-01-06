4c737138ba0106570069dd2236d5233f76f065d0
sakuow <yusaku1335@gmail.com>
2022-01-06 15:43:22 +0900

    正規表現part1

--- 040_PHP基礎Part2/0500_正規表現を使ってみよう/end/index.php
+++ 040_PHP基礎Part2/0500_正規表現を使ってみよう/end/index.php
@@ -21,10 +21,9 @@
  */
 
 $char = '<h2>1ZAabd12_sscc</h2>';
-if(preg_match("/<h2>(.{2,})<\/h2>/i", $char, $result)) {
+if (preg_match("/<h2>(.{2,})<\/h2>/i", $char, $result)) {
     echo '検索成功';
     print_r($result);
 } else {
     echo '検索失敗';
 }
- 
