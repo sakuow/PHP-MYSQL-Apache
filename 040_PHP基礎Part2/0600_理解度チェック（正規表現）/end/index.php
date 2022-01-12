cec4138e0de2fa8cb840bd543856780bd7272ba7
sakuow <yusaku1335@gmail.com>
2022-01-12 17:17:54 +0900

    コメント

--- 040_PHP基礎Part2/0600_理解度チェック（正規表現）/end/index.php
+++ 040_PHP基礎Part2/0600_理解度チェック（正規表現）/end/index.php
@@ -21,7 +21,6 @@
 
 /**
  * 郵便番号
- * 
  * 001-0012 -> OK
  * 001-001 -> NG
  * 2.2-3042 -> NG
@@ -39,7 +38,6 @@ if (preg_match("/^\d{3}-\d{4}$/", $zipcode, $result)) {
 /**
  * Email
  * . _ - と半角英数字が可能
- *
  * example000@gmail.com -> OK
  * example-0.00@gmail.com -> OK
  * example-0.00@ex.co.jp -> OK
